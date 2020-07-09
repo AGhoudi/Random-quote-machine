bb9667e6f5f4cdad6122fc16ebd9b860939bf921
Link1986 <ali.ghoudi@gmail.com>
2020-07-09 13:55:12 +0200

    Updates

---
+++ precache-manifest.1852862d02bf90a63638422be44afff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c92d33909bbfb546c0dcc87d3c865b",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "2bd279a04afa942eb65c",
+    "url": "/random-quote-machine/static/css/main.26057c12.chunk.css"
+  },
+  {
+    "revision": "ca1d8e817362999cbbdb",
+    "url": "/random-quote-machine/static/js/2.1c9bc77a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.1c9bc77a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd279a04afa942eb65c",
+    "url": "/random-quote-machine/static/js/main.d5fd5c56.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
